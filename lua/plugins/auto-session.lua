53f879cc7c2479bc139b3dae710d3c5366f4fffa
ambaradan <ambaradan@proton.me>
2024-06-06 12:53:58 +0200

    feat(add): enable auo-session

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,21 +1,23 @@
 return {
-  {
-    "rmagatti/auto-session",
-    lazy = false,
-    keys = {
-      { "<leader>qr", "<cmd>SessionRestore<CR>", desc = "Restore Session" },
-      { "<leader>qs", "<cmd>SessionSave<CR>", desc = "Save Session" },
-      { "<leader>qd", "<cmd>SessionDelete<CR>", desc = "Delete this Session" },
-      { "<leader>qf", "<cmd>Telescope session-lens search_session<CR>", desc = "Find sessions" },
-    },
-    opts = {
-      log_level = vim.log.levels.ERROR,
-      auto_session_enabled = true,
-      auto_save_enabled = true,
-      auto_restore_enabled = true,
-      auto_session_suppress_dirs = { "~/", "/", "~/Downloads", "~/Documents", "~/dev" },
-      auto_session_use_git_branch = true,
-      -- auto_session_enable_last_session = false,
-    },
-  },
+	{
+		"rmagatti/auto-session",
+		enabled = true,
+		lazy = false,
+		keys = {
+			{ "<leader>qr", "<cmd>SessionRestore<CR>", desc = "Restore Session" },
+			{ "<leader>qs", "<cmd>SessionSave<CR>", desc = "Save Session" },
+			{ "<leader>qd", "<cmd>SessionDelete<CR>", desc = "Delete this Session" },
+			{ "<leader>qf", "<cmd>Telescope session-lens search_session<CR>", desc = "Find sessions" },
+		},
+		config = function()
+			local auto_session = require("auto-session")
+
+			auto_session.setup({
+				auto_restore_enabled = true,
+				auto_session_suppress_dirs = { "~/Downloads", "~/Documents", "~/Desktop" },
+				auto_save_enabled = true,
+				auto_session_use_git_branch = true,
+			})
+		end,
+	},
 }
