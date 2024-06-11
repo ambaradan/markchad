4bc8eac0e05a8754ad90bfca6a16fab90bb8c83a
ambaradan <ambaradan@proton.me>
2024-06-11 22:07:57 +0200

    wip: add more notes

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,7 +9,7 @@ map("i", "jk", "<ESC>")
 
 -- Aligns the table cells to the width
 map("v", "<leader>ta", "!pandoc -t markdown-simple_tables<cr>", { silent = true, desc = "Align md table" })
-
+-- Command to close all buffers
 map("n", "<leader>cx", function()
   require("nvchad.tabufline").closeAllBufs()
 end, { desc = "Close All Buffers" })
