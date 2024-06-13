77cac003d24f4f0f89ee2f52901364fd6011090e
ambaradan <ambaradan@proton.me>
2024-06-13 11:29:16 +0200

    fix(opts): add auto-session opts - rm indent ops

--- lua/options.lua
+++ lua/options.lua
@@ -1,14 +1,7 @@
 require "nvchad.options"
 
--- add yours here!
+-- recommended options for auto-session
+vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
--- Indent Settings
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.smartindent = true
-vim.opt.cindent = true
-vim.opt.smarttab = true
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
