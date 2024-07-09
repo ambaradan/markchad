5fc02b1ca625eed9ea2e327e11b40331bb44b650
ambaradan <ambaradan@proton.me>
2024-07-09 11:31:04 +0200

    fix(conf): refactor conf - render.markdown
    Edit to reflect changes to new configuration of 'render-markdown.nvim'

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -3,28 +3,28 @@ return {
 	enabled = true,
 	latex_enabled = false,
 	name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-	--dependencies = { "nvim-treesitter/nvim-treesitter" },
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
 	ft = "markdown",
 	config = function()
 		require("render-markdown").setup({
-			-- Characters that will replace the # at the start of headings
-			headings = { "☶ ", "☴ ", "☱ ", "☳ ", "☲ ", "☷ " },
-			table_style = "none",
-			highlights = {
-				heading = {
-					-- Background of heading line
-					backgrounds = { "CmpItemKindType", "CmpItemKindText", "CmpItemKindConstant", "CmpItemKindValue", "CmpItemKindUnit", "CmpItemKindFolder" },
-					-- Foreground of heading character only
-					foregrounds = {
-						"markdownH1",
-						"markdownH2",
-						"markdownH3",
-						"markdownH4",
-						"markdownH5",
-						"markdownH6",
-					},
-				},
-				bullet = "CmpItemKindText",
+			heading = {
+				-- Replaces '#+' of 'atx_h._marker'
+				-- The number of '#' in the heading determines the 'level'
+				-- The 'level' is used to index into the array using a cycle
+				-- The result is left padded with spaces to hide any additional '#'
+				icons = { "☶ ", "☴ ", "☱ ", "☳ ", "☲ ", "☷ " },
+				-- Added to the sign column
+				-- The 'level' is used to index into the array using a cycle
+				signs = { "󰫎 " },
+				-- The 'level' is used to index into the array using a clamp
+				-- Highlight for the heading icon and extends through the entire line
+				backgrounds = { "Added", "Changed", "Character", "Boolean" },
+				-- The 'level' is used to index into the array using a clamp
+				-- Highlight for the heading and sign icons
+				foregrounds = { "markdownH1", "markdownH2", "markdownH3", "markdownH4", "markdownH5", "markdownH6" },
+			},
+			bullet = {
+				highlight = "Boolean",
 			},
 		})
 	end,
