8b7bb599ca8e83328bb6062489d639d253ba108c
ambaradan <ambaradan@proton.me>
2024-07-15 17:19:11 +0200

    feat(set): key for highlight 'mkdocs-material'

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -8,12 +8,13 @@ return {
 				local function toggle(key)
 					return "<Esc>gv<Cmd>lua require'markdown.inline'" .. ".toggle_emphasis_visual'" .. key .. "'<CR>"
 				end
-				-- mapping for INSERT mode (bold, italic, inline code)
+				-- mapping for INSERT mode (bold, italic, inline, highlight code)
 				vim.keymap.set("x", "<C-b>", toggle("b"), { buffer = bufnr })
 				vim.keymap.set("x", "<C-i>", toggle("i"), { buffer = bufnr })
 				vim.keymap.set("x", "<C-c>", toggle("c"), { buffer = bufnr })
-        vim.keymap.set("x", "<C-s>", toggle("s"), { buffer = bufnr })
+        vim.keymap.set("x", "<C-h>", toggle("s"), { buffer = bufnr })
 			end,
+      -- modified mapping for mkdocs-material 'highlight' tag
       inline_surround = {
         strikethrough = {
           text = "s",
