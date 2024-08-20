6391c13bec8b1a421fc37e090f26430cd0003fe0
ambaradan <ambaradan@proton.me>
2024-08-20 09:47:09 +0200

    feat: move term to bottom right

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,7 +10,15 @@ map("i", "jk", "<ESC>")
 -- replacement of standard nvimtree mapping with neotree
 map("n", "<C-n>", "<cmd>Neotree right toggle<CR>", { desc = "neotree toggle window" })
 map("n", "<leader>e", "<cmd>Neotree focus<CR>", { desc = "neotree focus window" })
-
+-- Custom floating term
+map({ "n", "t" }, "<A-i>", function()
+    require("nvchad.term").toggle { pos = "float", id = "floatTerm", float_opts={
+        row = 0.45,
+        col = 0.40,
+        width = 0.6,
+        height = 0.4
+    }}
+end, { desc = "terminal toggle floating term" })
 -- Aligns the table cells to the width
 map("v", "<leader>ta", "!pandoc -t markdown-simple_tables<cr>", { silent = true, desc = "Align md table" })
 -- Command to close all buffers
