b2b1c976d96c0188b873359fdaca6186164a7b01
ambaradan <ambaradan@proton.me>
2024-08-26 23:42:07 +0200

    wip: work on remove folders before install

--- scripts/INSTALL.sh
+++ scripts/INSTALL.sh
@@ -167,34 +167,59 @@ done
 clear
 title_green "$title_script"
 printf "\n"
-
 # Configurazione delle variabili
-conf_dir=".config/$root_dir"
-share_dir=".local/share/$root_dir"
-cache_dir=".cache/$root_dir"
-
-date=$(date +%m-%d-%Y-%H-%M)
+config="$HOME/.config/$root_dir"
+share_local="$HOME/.local/share/$root_dir"
+cache_dir="$HOME/.cache/$root_dir"
+backup_nvim="$HOME/backup/$root_dir"
 cd "$HOME" || exit
-if [ -d "$conf_dir" ]; then
+if [ -d "$config" ]; then
   while true; do
     section_title "$backup_title"
     format_text "$backup_info"
     printf "\n"
-    printf "%s\n" "$backup_paths" | indent 2
-    printf "${orange}%s${clear}\n" "$HOME/$conf_dir" | indent 4
-    printf "${orange}%s${clear}\n\n" "$HOME/$share_dir" | indent 4
-    printf "%s\n" "$backup_save_path" | indent 2
-    printf "%s\n" "$HOME/backup" | indent 4
+    printf "${bold_in}%s${bold_out}\n" "$backup_paths" | indent 2
+    printf "${orange}%s${clear}\n" "$config" | indent 4
+    printf "${orange}%s${clear}\n\n" "$share_local" | indent 4
+    printf "${bold_in}%s${bold_out}" "$backup_save_path" | indent 2
+    printf "${orange}%s${clear}\n" "$HOME/backup/$root_dir" | indent 4
     printf "\n"
-    read -r -p " Start the backup? (y/n) " yn
+    read -r -p "  Start the backup? (y/n) " yn
     case $yn in
     [Yy]*)
-      # Backup della configurazione esistente
-      mkdir -p ~/backup/"$root_dir-$date"
-      printf "${orange}%s${clear}" "$root_dir" "$HOME/backup/$root_dir-$date"
-      cp -r "$conf_dir" ~/backup/"$root_dir"-"$date"/"$date"-config/
-      cp -r "$share_dir" ~/backup/"$root_dir"-"$date"/"$date"-share/
-      confirm
+      mkdir -p "$backup_nvim"
+      # Copy the first source directory to a subdirectory in the destination directory
+      section_title "Copying files"
+      if [ -d "$config" ]; then
+        rsync -av --quiet --delete "$config" "$backup_nvim/config"
+        rsync_status1=$?
+        if [ $rsync_status1 -eq 0 ]; then
+          printf "${bold_in}Copy:${bold_out} %s\n" "$config" | indent 4
+          printf "  ${bold_in}to:${bold_out} %s/config\n" "$backup_nvim" | indent 4
+        else
+          printf "Copy of %s to %s/config failed with status %s" "$config" "$backup_nvim" "$rsync_status1"
+        fi
+      else
+        printf "Source directory %s does not exist" "$config"
+      fi
+      # Copy the second source directory to a subdirectory in the destination directory
+      if [ -d "$share_local" ]; then
+        rsync -av --quiet --delete "$share_local" "$backup_nvim/share_local"
+        rsync_status2=$?
+        if [ $rsync_status2 -eq 0 ]; then
+          printf "${bold_in}Copy:${bold_out} %s\n" "$share_local" | indent 4
+          printf "  ${bold_in}to:${bold_out} %s/share_local\n" "$backup_nvim" | indent 4
+        else
+          printf "Copy of %s to %s failed with status %s" "$share_local" "$backup_nvim/share_local" "$rsync_status2"
+        fi
+      else
+        printf "Source directory %s does not exist" "$share_local"
+      fi
+      divider_single_green
+      center_bold_green "Backup performed correctly"
+      divider_single_green
+      printf "\n  Press any key to continue.."
+      press_to_continue
       break
       ;;
     [Nn]*)
@@ -203,69 +228,57 @@ if [ -d "$conf_dir" ]; then
     *) echo "Please answer Y/y or N/n" ;;
     esac
   done
-  printf "  Press any key to continue.."
-  press_to_continue
   # Rimozione della installazione precedente
   clear
   title_green "$title_script"
   printf "\n"
-  printf " ${bold_in}%s${bold_out}\n" "Removing previous installations" | indent 2
-  divider
-  printf " Removing folder ${orange}%s${clear}: " "$conf_dir" | indent 4
-  rm -rf "$conf_dir"
-  confirm
-  if [ -d "$share_dir" ]; then
-    printf " Removing folder ${orange}%s${clear}: " "$share_dir" | indent 4
-
-    rm -rf "$share_dir"
-    confirm
+  section_title "Removing previous installations"
+  printf "Removing folder: \n" | indent 2
+  printf "${orange}%s${clear}" "$config" | indent 4
+  rm -rf "$config"
+  if [ -d "$share_local" ]; then
+    printf "${orange}%s${clear}" "$share_local" | indent 4
+    rm -rf "$share_local"
   else
-    printf " Directory %s not present: \e[32mOK\e[0m\n" "$share_dir" | indent 4
-
+    printf " Directory %s not present: \e[32mOK\e[0m\n" "$share_local" | indent 4
   fi
   if [ -d "$cache_dir" ]; then
-    printf " Removing folder ${orange}%s${clear}: " "$cache_dir" | indent 4
-
+    printf "${orange}%s${clear}: " "$cache_dir" | indent 4
     rm -rf "$cache_dir"
-    confirm
   else
     printf " Directory ${orange}%s${clear} not present: \e[32mOK\e[0m\n" "$cache_dir" | indent 4
 
   fi
 fi
-printf "  Press any key to continue.."
-press_to_continue
-printf " ${bold_in}%s${bold_out} ${orange}%s${clear}\n" "Start configuration installation" "$root_dir"
-divider
+section_title "Start configuration installation"
+printf "\n"
 printf "${bold_in}%s\n${bold_out}" " Downloading the latest version of the configuration"
 curl -L https://github.com/ambaradan/markchad/releases/latest/download/markchad.tar.gz --output $tmp_dir/markchad.tar.gz
-divider
-printf "${bold_in}%s\n${bold_out}" " Extraction of compressed archive: "
-tar -xf $tmp_dir/markchad.tar.gz --checkpoint=.1650 --checkpoint-action=dot -C $tmp_dir
-confirm
-divider
-printf " ${bold_in}%s\n${bold_out}" "Setup installation"
+printf "\n${bold_in}%s\n${bold_out}" " Extraction of compressed archive: "
+tar -xf $tmp_dir/markchad.tar.gz --checkpoint=.1250 --checkpoint-action=dot -C $tmp_dir
+section_title "Setup installation"
 printf " Copying files: "
-cp -r $tmp_dir/markchad/config/nvim/ "$conf_dir"
-cp -r $tmp_dir/markchad/share/nvim/ "$share_dir"
-confirm
-printf " Configuration files copied to: \n\t${orange}%s${clear}\n" "$HOME/$conf_dir"
-printf " Shared files copied to:\n\t${orange}%s${clear}\n" "$HOME/$share_dir"
-printf " Cleaning temporary files: "
+cp -r $tmp_dir/markchad/config/nvim/ "$config"
+cp -r $tmp_dir/markchad/share/nvim/ "$share_local"
+printf " Configuration files copied to: \n"
+printf "${orange}%s${clear}\n" "$HOME$config"
+printf " Shared files copied to: \n"
+printf "${orange}%s${clear}\n" "$HOME$share_local"
+section_title "Cleaning temporary files"
 rm -rf $tmp_dir/markchad
 rm -f $tmp_dir/markchad.tar.gz
 if [ -z "$(ls -A $tmp_dir)" ]; then
   rm -rf ~/.local/tmp/
-  confirm
+  printf "Folder .local/tmp removed\n"
 else
   printf "\n\t${orange}%s${clear} folder not empty: ${orange}%s${clear}\n" "$HOME/$tmp_dir" "Skipped"
 fi
-divider
-printf "\t${bold_in}${green}%s${clear}${bold_out}\n" "Installation performed properly"
-divider
+divider_single_green
+center_bold_green "Installation performed properly"
+divider_single_green
 if [ "$root_dir" == "markchad" ]; then
-  printf " ${red}%s${clear}\n To start this version of the configuration, it is necessary to use\n for further starts the variable ${blue}%s${clear}\n\n" "NOTE:" "NVIM_APPNAME"
-  printf " ${blue}%s${clear}\n\n" "NVIM_APPNAME=markchad nvim"
+  format_text "To start this version of the configuration, it is necessary to use\n for further starts the variablen"
+  printf "\n${blue}%s${clear}\n" "NVIM_APPNAME=markchad nvim" | indent 4
 else
   printf " To start the new Neovim configuration use the command ${blue}%s${clear}\n\n" "nvim"
 fi
