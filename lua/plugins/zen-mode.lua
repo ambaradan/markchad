cfcfca2389e5fa80c478769e09367df43ba10985
ambaradan <ambaradan@proton.me>
2024-10-04 23:43:44 +0200

    wip: test folke/zen-mode.nvim

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -1,19 +1,18 @@
+-- Lua
 return {
-	"Manas140/Zazen.nvim",
+	"folke/zen-mode.nvim",
 	enabled = true,
-	cmd = "Zazen",
-	keys = {
-		{
-			"<leader>zm",
-			"<cmd>Zazen<cr>",
-			desc = "Zen Mode",
+	cmd = "ZenMode",
+	opts = {
+		window = {
+			options = {
+				number = false,
+				relativenumber = false,
+			},
+		},
+		kitty = {
+			enabled = true,
+			font = "+4", -- font size increment
 		},
 	},
-	config = function()
-		require("zazen").setup({
-			num = false, -- show numbers
-			gap = 0.2, -- prefered to keep value under 0.5
-			border = "none",
-		})
-	end,
 }
