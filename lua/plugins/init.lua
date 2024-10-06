1abf2547742dcc951a6642a4b56e9e1793ccd8b4
ambaradan <ambaradan@proton.me>
2024-10-06 11:01:14 +0200

    fix: adjusted the code based on 'starter'

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,16 +2,13 @@ return {
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
-    config = function()
-      require "configs.conform"
-    end,
+    opts = require "configs.conform",
   },
 
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
-      require("nvchad.configs.lspconfig").defaults()
       require "configs.lspconfig"
     end,
   },
