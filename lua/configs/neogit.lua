925fbef8d6e5658da19f7c0bb167753563b2a9bf
ambaradan <ambaradan@proton.me>
2024-10-09 13:58:43 +0200

    wip: set commit_view floating

--- lua/configs/neogit.lua
+++ lua/configs/neogit.lua
@@ -1,12 +1,16 @@
 require("neogit").setup({
-  kind = "auto",
-  disable_builtin_notifications = true,
-  integrations = {
-    telescope = true,
-    diffview = true,
-  },
-  status = {
-    -- show_head_commit_hash = true,
-    recent_commit_count = 20,
-  },
+	kind = "auto",
+	disable_builtin_notifications = true,
+	integrations = {
+		telescope = true,
+		diffview = true,
+	},
+	status = {
+		-- show_head_commit_hash = true,
+		recent_commit_count = 20,
+	},
+	commit_view = {
+		kind = "floating",
+		verify_commit = vim.fn.executable("gpg") == 1,
+	},
 })
