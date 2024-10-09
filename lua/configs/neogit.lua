78ac12de3d33d33519e4e345ea5be47e9c572ca1
ambaradan <ambaradan@proton.me>
2024-10-09 14:34:00 +0200

    wip: set more 'kind' views

--- lua/configs/neogit.lua
+++ lua/configs/neogit.lua
@@ -1,7 +1,7 @@
 require("neogit").setup({
 	kind = "auto",
 	disable_builtin_notifications = true,
-  graph_style = "unicode",
+	graph_style = "unicode",
 	integrations = {
 		telescope = true,
 		diffview = true,
@@ -13,5 +13,11 @@ require("neogit").setup({
 	commit_view = {
 		kind = "floating",
 		verify_commit = vim.fn.executable("gpg") == 1,
+	},
+	commit_editor = {
+		kind = "floating",
+	},
+	log_view = {
+		kind = "floating",
 	},
 })
