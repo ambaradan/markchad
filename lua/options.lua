671a3b0ca4d176f5acdbd55f8f7292acff58054d
ambaradan <ambaradan@proton.me>
2024-10-20 20:23:14 +0200

    wip: add folding headers

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,7 @@
 require "nvchad.options"
 
 -- add yours here!
-
+-- Allow for folding markdown headings
+vim.g.markdown_folding = 1
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
