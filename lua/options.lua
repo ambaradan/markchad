a7bebcb6874e2aebad2010e853a4e5be98035511
ambaradan <ambaradan@proton.me>
2024-10-22 12:29:53 +0200

    fix: remove markdown folding

--- lua/options.lua
+++ lua/options.lua
@@ -2,6 +2,6 @@ require "nvchad.options"
 
 -- add yours here!
 -- Allow for folding markdown headings
-vim.g.markdown_folding = 1
+-- vim.g.markdown_folding = 1
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
