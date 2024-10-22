d4770a3b06b88d551856f0a6b16f98b9af040d16
ambaradan <ambaradan@proton.me>
2024-10-22 10:00:52 +0200

    wip: testing vim.lua.loader

--- init.lua
+++ init.lua
@@ -1,3 +1,9 @@
+-- Enable faster lua loader using byte-compilation
+-- https://github.com/neovim/neovim/commit/2257ade3dc2daab5ee12d27807c0b3bcf103cd29
+pcall(function()
+  vim.loader.enable()
+end)
+
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
 
